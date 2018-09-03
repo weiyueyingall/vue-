3f55ad1615003617eb55428134a422fabf475d8f
weiyueying <493454974@qq.com>
2018-09-03 15:33:34 +0800

    home样式完成

--- itcast_store50/src/router/index.js
+++ itcast_store50/src/router/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import login from '@/views/login.vue';
+
+import home from '@/views/home.vue';
 // import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
-        { name: 'home', path: '/', redirect: '/login' },
+        { name: 'home', path: '/', component: home },
         { name: 'login', path: '/login', component: login }
     ]
 });
